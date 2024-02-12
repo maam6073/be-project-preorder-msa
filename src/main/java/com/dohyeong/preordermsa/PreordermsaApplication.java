9035394816495a924cd90a381fe26648c66b6181
maam6073 <maam10@naver.com>
2024-02-12 16:15:57 +0900

    Initial commit

---
+++ src/main/java/com/dohyeong/preordermsa/PreordermsaApplication.java
@@ -0,0 +1,13 @@
+package com.dohyeong.preordermsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreordermsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreordermsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dohyeong/preordermsa/PreordermsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dohyeong.preordermsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreordermsaApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
